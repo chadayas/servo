1fdf66d3d4bdeccc2aa1dc458fc972deba8a3aea
chadayas <cgassoya@gmail.com>
2026-03-05 15:12:30 -0500

    yml for servo lib

--- main/servo.cpp
+++ main/servo.cpp
@@ -1,14 +1,29 @@
 #include <stdio.h>
+#include "driver/ledc.h"
+#include "driver/gpio.h"
+#include "esp_err.h"
+#include "iot_servo.h"
+#include "esp_log.h"
 
-void app_main(void)
+
+
+#define ANGLE  120.0f
+
+
+extern "C" void app_main(void)
 {
 	servo_config_t s_cfg{};
 	s_cfg.max_angle = 270;
 	s_cfg.min_width_us = 500;
 	s_cfg.max_width_us = 2500;
+	s_cfg.freq = 50;	
 	s_cfg.timer_number = LEDC_TIMER_0;
-	s_cfg.channel_number = LEDC_CHANNEL_0;
-	s_cfg.gpio_num = GPIO_NUM_14;
+	s_cfg.channel_number = 1;
+	s_cfg.channels.servo_pin[0] = GPIO_NUM_14;
+	s_cfg.channels.ch[0] = LEDC_CHANNEL_0;
+	
+	iot_servo_init(LEDC_LOW_SPEED_MODE, &s_cfg) 
+	iot_servo_write_angle(LEDC_LOW_SPEED_MODE, 0, ANGLE);
 
-
+	iot_servo_read_angle(LEDC_LOW_SPEED_MODE, 0, &ANGLE);
 }
