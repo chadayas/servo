6e13723e464a2d0d5da67a10a555ab8805572619
chadayas <cgassoya@gmail.com>
2026-03-06 21:00:00 -0500

    controlling servo with arrow keys using getchar()

--- main/servo.cpp
+++ main/servo.cpp
@@ -21,21 +21,23 @@ extern "C" void app_main(void)
 	s_cfg.channels.servo_pin[0] = GPIO_NUM_14;
 	s_cfg.channels.ch[0] = LEDC_CHANNEL_0;
 	
-	float angle  = 120.0;
-
-
 	iot_servo_init(LEDC_LOW_SPEED_MODE, &s_cfg); 
-	iot_servo_write_angle(LEDC_LOW_SPEED_MODE, 0, angle);
-
+	
+	float angle = 0.0;
 	auto tag = "[--ANGLE--]";	
 	while(1){
-		for(int angle = 0; angle <= 200; angle++){
-			iot_servo_write_angle(LEDC_LOW_SPEED_MODE, 0, angle );
-			if (iot_servo_read_angle(LEDC_LOW_SPEED_MODE, 0, &angle) == ESP_OK){
-				ESP_LOGI(tag,": %.1f", angle);	
-			}
-			vTaskDelay(100/ portTICK_PERIOD_MS);
+		char ch = getchar();
+		if (ch == 27){
+			angle += 1.0;
+			iot_servo_write_angle(LEDC_LOW_SPEED_MODE, 0, angle);
+		} else if(ch == 26){
+			angle =- 1.0;
+			iot_servo_write_angle(LEDC_LOW_SPEED_MODE, 0, angle);
 		}	
+		vTaskDelay(5/ portTICK_PERIOD_MS);
+
+
+			
 	}
 
 }
